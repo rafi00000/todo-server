9e835eceb315e8ff8ddfd5dedd3efcbfc4787524
Rafi <delwar2021bd@gmail.com>
2023-12-25 12:57:42 +0600

    added vercel task update api and delete api

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+require('dotenv').config()
+
 
 // middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://console.firebase.google.com"],
   })
 );
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://rafi2021bd:koajaibona1@cluster0.irfnbkn.mongodb.net/?retryWrites=true&w=majority";
+const uri =`${process.env.MONGO_URI}`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,24 +29,66 @@ async function run() {
   try {
     const taskCollection = client.db("TODODb").collection("todo");
 
-    app.get("/task", async (req, res) => {
-      const email = req.query.email;
-      console.log(email)
-      let result;
-      if (email) {
-        result = await taskCollection.find({ email: email }).toArray();
-      } else {
-        result = await taskCollection.find().toArray();
-      }
-      res.send(result); 
+    app.get("/task/:email", async (req, res) => {
+       const email = req.params.email;
+       const query = {email: email};
+       console.log(email)
+       const result = await taskCollection.find(query).toArray();
+       console.log(result)
+       res.send(result);
     });
+
+    app.get("/task/single/:id", async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post("/task", async (req, res) => {
       const data = req.body;
-      console.log(data);
+      // console.log(data);
       const result = await taskCollection.insertOne(data);
       res.send(result);
     });
+
+    app.delete("/task/:id", async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put("/task/:id",async(req, res) =>{
+      const id = req.params.id;
+      const doc = req.body;
+      console.log(doc)
+      const query = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          title: doc.title,
+          description: doc.description,
+          deadline: doc.deadline,
+          priority: doc.priority,
+        }
+      };
+      const result = await taskCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/task/:id", async(req, res) =>{
+      const id = req.params.id;
+      const doc = req.body;
+      console.log(doc, id)
+      const query = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          status: doc.status
+        }
+      }
+      const result = await taskCollection.updateOne(query, updateDoc);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
